78be36f3cf60e8f42c0acdf90179f455118bdccf
Mahmudul Hassan Navid <mahmudulhassannavid@gmail.com>
2025-03-23 16:30:29 +0100

    feat: researcher agent added to research on subtopics. chat result caching added. refactor: planner agent code refactored.

--- planner_agent.py
+++ planner_agent.py
@@ -1,25 +1,11 @@
-from langchain.chat_models import init_chat_model
 from langchain_core.prompts import PromptTemplate
-from langchain_core.globals import set_llm_cache
-from langchain_core.caches import InMemoryCache
-
-from config import MODEL_NAME, TOGETHER_API_KEY, MODEL_PROVIDER
-
-model = init_chat_model(
-    model=MODEL_NAME,
-    api_key=TOGETHER_API_KEY,
-    model_provider=MODEL_PROVIDER,
-    temperature=0.7,
-)
-
-set_llm_cache(InMemoryCache())
 
 prompt_template = PromptTemplate.from_template(
     "I'm providing a research topic. Break the topic: '{topic}' into 2 subtopics for in-depth research. Provide only the name of the subtopics."
 )
 
 
-def planner_agent(topic: str) -> list[str]:
+def planner_agent(model: any, topic: str) -> list[str]:
     prompt = prompt_template.format_prompt(topic=topic)
     response = model.invoke(prompt)
 
